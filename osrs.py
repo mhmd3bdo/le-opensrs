7eeff19c966452b1ef654182291bb825a3d18839
mhmd3bdo <mhmd3bdo@gmail.com>
2019-02-11 23:02:26 +0200

    latest state

--- osrs.py
+++ osrs.py
@@ -1,23 +1,29 @@
+import configparser
 import requests
 import hashlib
-import xml.etree.ElementTree as ET
+import logging
+import xml.etree.ElementTree as et
+
+
+log = logging.getLogger('le-opensrs')
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.INFO)
 
 def _update_dns(domain,challenge):
-
     def text_element(parent, tag, text, *args, **kwargs):
-        element = ET.SubElement(parent, tag, *args, **kwargs)
+        element = et.SubElement(parent, tag, *args, **kwargs)
         element.text = text
         return element
 
     def xml_request(domain, action, data=''):
-        root = ET.Element("OPS_envelope")
-        header = ET.Element("header")
-        body = ET.Element("body")
-        version = ET.Element("version")
-        data_block = ET.Element("data_block")
-        dt_assoc = ET.Element("dt_assoc")
-        attr_dt_assoc = ET.Element("dt_assoc")
-        item_attributes = ET.Element("item")
+        root = et.Element("OPS_envelope")
+        header = et.Element("header")
+        body = et.Element("body")
+        version = et.Element("version")
+        data_block = et.Element("data_block")
+        dt_assoc = et.Element("dt_assoc")
+        attr_dt_assoc = et.Element("dt_assoc")
+        item_attributes = et.Element("item")
         item_attributes.attrib = {'key': 'attributes'}
         version.text = '0.9'
         root.append(header)
@@ -32,8 +38,8 @@ def _update_dns(domain,challenge):
         item_attributes.append(attr_dt_assoc)
         text_element(attr_dt_assoc, 'item', domain, attrib={'key': 'domain'})
         if data != '':
-            attr_dt_assoc.append(res_item)
-        xml = ET.tostring(root, encoding="unicode", method="xml")
+            attr_dt_assoc.append(records)
+        xml = et.tostring(root, encoding="unicode", method="xml")
         xml = ("\n"
                "<?xml version=\'1.0\' encoding=\'UTF-8\' standalone=\'no\'?>\n"
                "<!DOCTYPE OPS_envelope SYSTEM \'ops.dtd\'>\n" + xml)
@@ -41,72 +47,84 @@ def _update_dns(domain,challenge):
 
     def xml_header(xml):
         md5_obj = hashlib.md5()
-        md5_obj.update((xml + connection_details['api_key']).encode('utf-8'))
+        md5_obj.update((xml + config['opensrs']['api_key']).encode('utf-8'))
         signature = md5_obj.hexdigest()
 
         md5_obj = hashlib.md5()
-        md5_obj.update((signature + connection_details['api_key']).encode('utf-8'))
+        md5_obj.update((signature + config['opensrs']['api_key']).encode('utf-8'))
         signature = md5_obj.hexdigest()
 
-        headers = {'Content-Type': 'text/xml', 'X-Username': connection_details['reseller_username'],
+        headers = {'Content-Type': 'text/xml', 'X-Username': config['opensrs']['reseller_username'],
                    'X-Signature': signature, };
         return headers
 
     # main
     action = 'get_dns_zone'
-
-
-    TEST_MODE = 1
-    connection_options = {'live': {# IP whitelisting required
-        'reseller_username': 'zaincloud',
-        'api_key': '0261e22a98a7017ca635ef68161f12cd5320fb1f1cc6f12b943e574a8b090cc58d02c48b17b049d4364dc3a13b29b7d57fd711343bfc8492',
-        'api_host_port': 'https://rr-n1-tor.opensrs.net:55443', }, 'test': {# IP whitelisting not required
-        'reseller_username': 'zaincloud',
-        'api_key': '8fdc8834a8e27b73cd132632324ad87d25c9ccd116c7e2b94229915bf5c25f56a59394b13ab16e7caa67aefee2fd076d12aaf5c5fa78fbc2',
-        'api_host_port': 'https://horizon.opensrs.net:55443',
-
-    }}
-
-    if TEST_MODE == 1:
-        connection_details = connection_options['test']
-    else:
-        connection_details = connection_options['live']
-
+    config = configparser.ConfigParser()
+    config.read('config')
     xml = xml_request(domain, action)
     headers = xml_header(xml)
-    print("Request to {} as reseller {}:".format(connection_details['api_host_port'],
-                                                 connection_details['reseller_username']))
-    print(xml)
-
-    r = requests.post(connection_details['api_host_port'], data=xml, headers=headers)
-
-    print("Response:")
-    if r.status_code == requests.codes.ok:
-        print(r.text)
+    log.info("Request to {} as reseller {}:".format(config['opensrs']['api_host_port'],
+                                                    config['opensrs']['reseller_username']))
+    log.info(xml)
+    r = requests.post(config['opensrs']['api_host_port'], data=xml, headers=headers)
+    log.info("Response:")
+    log.info(r.text)
+    res = et.fromstring(r.text)
+    dt_assoc = et.Element("dt_assoc")
+    dt_array = et.Element("dt_array")
+    item_rcd = et.Element("item")
+    record_exist = False
+    records = res.find("./body/data_block/dt_assoc/item/[@key='attributes']/dt_assoc/item/[@key='records']")
+    txt_records = res.findall(
+        "./body/data_block/dt_assoc/item/[@key='attributes']/dt_assoc/item/[@key='records']/dt_assoc/item/[@key='TXT']/dt_array/item")
+    txt_index = len(txt_records)
+    txt_record = res.find(
+        "./body/data_block/dt_assoc/item/[@key='attributes']/dt_assoc/item/[@key='records']/dt_assoc/item/[@key='TXT']/dt_array")
+    if txt_index == 0:
+        log.info("There is no TXT record resources ... will be added")
+        res_item = res.find(
+            "./body/data_block/dt_assoc/item/[@key='attributes']/dt_assoc/item/[@key='records']/dt_assoc")
+        item_rcd.attrib = {'key': 'records'}
+        item_rcd.append(res_item)
+        item_txt = et.Element("item")
+        item_txt.attrib = {'key': 'TXT'}
+        item_txt.append(dt_array)
+        res_item.append(item_txt)
+        item_n = et.Element("item")
+        item_n.attrib = {'key': '{0}'.format(txt_index)}
+        item_n.append(dt_assoc)
+        text_element(dt_assoc, 'item', '_acme-challenge', attrib={'key': 'subdomain'})
+        text_element(dt_assoc, 'item', challenge, attrib={'key': 'text'})
+        dt_array.append(item_n)
     else:
-        print(r.status_code)
-        print(r.text)
-
-    res = ET.fromstring(r.text)
-    res_item = res.find("./body/data_block/dt_assoc/item/[@key='attributes']/dt_assoc/item/[@key='records']")
-    acme = res.find(
-        "./body/data_block/dt_assoc/item/[@key='attributes']/dt_assoc/item/[@key='records']/dt_assoc/item/[@key='TXT']/dt_array/item/dt_assoc/item/[@key='text']")
-    print(acme.text)
-    acme.text = challenge
+        log.info("TXT record resources are exist")
+        for x in txt_records:
+            f = x.find("./dt_assoc/item/[@key='subdomain']").text
+            if f == '_acme-challenge':
+                record_exist = True
+                log.info("found previous TXT record for acme .... will attempts to update it")
+                pointer = x.attrib
+                break
+        if record_exist:
+            acme = records.find(
+                "./dt_assoc/item/[@key='TXT']/dt_array/item/[@key='{0}']/dt_assoc/item/[@key='text']".format(
+                    pointer['key']))
+            acme.text = challenge
+        else:
+            log.info("install new acme TXT record")
+            item_n = et.Element("item")
+            item_n.attrib = {'key': '{0}'.format(txt_index)}
+            item_n.append(dt_assoc)
+            text_element(dt_assoc, 'item', '_acme-challenge', attrib={'key': 'subdomain'})
+            text_element(dt_assoc, 'item', challenge, attrib={'key': 'text'})
+            txt_record.append(item_n)
     action = 'set_dns_zone'
-    xml = xml_request(domain, action, res_item)
+    xml = xml_request(domain, action, records)
     headers = xml_header(xml)
-    print("Request to {} as reseller {}:".format(connection_details['api_host_port'],
-                                                 connection_details['reseller_username']))
-    print(xml)
-
-    r = requests.post(connection_details['api_host_port'], data=xml, headers=headers)
-
-    print("Response:")
-    if r.status_code == requests.codes.ok:
-        print(r.text)
-    else:
-        print(r.status_code)
-        print(r.text)
-
-    return (r)
+    log.info("Request to {} as reseller {}:".format(config['opensrs']['api_host_port'],
+                                                    config['opensrs']['reseller_username']))
+    log.info(xml)
+    r = requests.post(config['opensrs']['api_host_port'], data=xml, headers=headers)
+    log.info("Response:")
+    log.info(r.text)
